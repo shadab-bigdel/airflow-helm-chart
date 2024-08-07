842ab331d119d7d736d095d2daec457f09f35d09
Shadab Bigdel <101270730+shadab-bigdel@users.noreply.github.com>
2024-08-07 14:39:26 +0200

    Update hello_world_Gitsync_dag.py
    Correction hello world gitsync dag

--- dags/hello_world_Gitsync_dag.py
+++ dags/hello_world_Gitsync_dag.py
@@ -2,16 +2,16 @@ from airflow import DAG # type: ignore
 from airflow.operators.python import PythonOperator # type: ignore
 from datetime import datetime
 
-def helloWorldGitsync():
-    print("Hello World Gitsync")
+def helloWorld():
+    print("Hello World Git Sync")
 
 with DAG(dag_id="hello_world_Gitsync_dag",
          start_date=datetime(2021,1,1),
          schedule_interval="@hourly",
-         catchup=False)
+         catchup=False) as dag:
     
     task1 = PythonOperator(
         task_id="hello_world_Gitsync",
-        python_callable=helloWorldGitsync)
+        python_callable=helloWorld)
     
 task1
